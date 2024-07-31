5a5d75c0f98a0ecff69c1ee79fcb8a547a402e60
Konstantin Dushin <satsoft@mail.ru>
2024-07-31 15:16:37 +0300

    Добавлен функционал вывода персонов

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Person person1=new Person("Игорёк",12);
+        Person person2=new Person("Василий",46);
+
         System.out.println("Вывод информации");
         System.out.println("Дорабатываем Вывод информации");
         System.out.println("Доработка вторая Вывод информации");
+
+        System.out.println(person1);
     }
-
-    Person person1=new Person("Игорёк",12);
-    Person person2=new Person("Василий",46);
-
-
 }
